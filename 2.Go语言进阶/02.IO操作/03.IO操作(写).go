bd9e821a4b9ef505dd73b4d9169cfdd09c28e8c5
liyang <liyangcoder@163.com>
2021-01-17 14:48:28 +0800

    I/O写操作

--- 2.Go语言进阶/02.IO操作/03.IO操作(写).go
+++ 2.Go语言进阶/02.IO操作/03.IO操作(写).go
@@ -1,1 +1,22 @@
 package main
+
+import "os"
+
+func main() {
+	fileName := "./demo3.txt"
+	file, err := os.OpenFile(fileName, os.O_CREATE|os.O_WRONLY|os.O_APPEND, os.ModePerm)
+	if err != nil {
+		panic(err.Error())
+	}
+	defer file.Close()
+	//将字节数组写入文件
+	_, err = file.Write([]byte("file.Write:hello world\n"))
+	if err != nil {
+		panic(err.Error())
+	}
+	//将字符串写入文件
+	_, err = file.WriteString("file.WriteString:hello world\n")
+	if err != nil {
+		panic(err.Error())
+	}
+}
